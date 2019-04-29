09ea282384ee0699a81e70b444ea0031ac0615ad
Justin Linn <justinslinn727@gmail.com>
2019-04-28 22:49:42 -0400

    Delete item

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -31,6 +31,15 @@ const Mutations = {
       info
     );
   },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // find the item
+    const item = await ctx.db.query.item({ where }, `{id title}`);
+    // check for permissions
+    // TODO
+    // perform delete action
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
 };
 
 module.exports = Mutations;
